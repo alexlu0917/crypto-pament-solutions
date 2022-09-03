7fa85d4912180762057b6da576bba2588c9a2c47
alexlu0917 <alexlu0917.dev@gmail.com>
2022-09-03 13:16:23 +0100

    add bitcoin router to main routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import * as controller from "../controllers/index";
 import ethereumRouter from "./ethereum";
+import bitcoinRouter from "./bitcoin";
 
 export const index = Router();
 
 index.get("/", controller.index);
 index.use("/ethereum", ethereumRouter);
+index.use("/bitcoin", bitcoinRouter);
 
 
